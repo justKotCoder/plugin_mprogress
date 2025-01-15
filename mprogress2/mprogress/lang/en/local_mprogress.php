8de7a9c4d231c836fb742946a98d1747595cb3b0
1hiro <1hirokoae@gmail.com>
2025-01-15 22:28:27 +0500

    15.01.2025 COMMIT 2 (backend)

--- mprogress2/mprogress/lang/en/local_mprogress.php
+++
@@ -1,7 +0,0 @@
-<?php
-$string['pluginname'] = 'MProgress';
-$string['dashboard'] = 'Progress Dashboard';
-$string['view'] = 'View your progress';
-$string['modulename_assign'] = 'Assignment';
-$string['modulename_quiz'] = 'Quiz';
-$string['modulename_forum'] = 'Forum';
